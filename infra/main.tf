b30066da49241b0b2492586dc5a9eebfc04c502f
Saurav <sauravads123@gmail.com>
2021-08-11 17:54:50 +0545

    infra : basif rds usage

--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,25 @@ module "beanstalk_environment_go_infra_dev" {
       beanstalk_environment_name = "go-infra-dev"   
       beanstalk_environment_solution_stack_name=var.beanstalk_environment_solution_stack_name
 }
+
+module "rds_go_infra_dev" {
+      source = "./modules/rds"
+      allocated_storage    = 20
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t2.micro"
+  apply_immediately = true
+  name                 = "aws_test"
+  port = 3306
+  username             = "FAKE_USER"
+  password             = "FAKE_PASSWORD"
+  # CHECK THIS
+#   parameter_group_name = var.rds_parameter_group_name
+  # NEED TO RESEARCH MORE ABOUT THESE THEREE
+  db_subnet_group_name = "default-vpc-*"
+  # NEED MORE RESEARCH WHY EBS Inbound rules need to be addded to default 
+  vpc_security_group_ids = ["CREATE YOUR OWN SECURITY GROUP MAY BE" ]
+  skip_final_snapshot  = true
+  deletion_protection = true
+    
+}
