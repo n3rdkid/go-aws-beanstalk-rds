4dddfddf67cd707dbb7c56ba9bcab5ac87f5c07c
Saurav <sauravads123@gmail.com>
2021-08-12 17:05:58 +0545

    minor : added beanstalk env output

---
+++ infra/modules/beanstalk_environment/outputs.tf
@@ -0,0 +1,3 @@
+output "beanstalk_environment_endpoint" {
+    value=aws_elastic_beanstalk_environment.beanstalk_environment.endpoint_url
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,3 +1,6 @@
 output "rds_db_host" {
   value = module.rds_go_infra_dev.rds_db_host
 }
+output "beanstalk_env_host" {
+  value = module.beanstalk_environment_go_infra_dev.beanstalk_environment_endpoint
+}
