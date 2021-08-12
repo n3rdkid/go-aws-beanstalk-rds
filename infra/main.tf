5d4ebbf1e642548beeae091b95e02d9fecf7e936
Saurav <sauravads123@gmail.com>
2021-08-12 11:51:41 +0545

    infra : deployment test

--- infra/main.tf
+++ infra/main.tf
@@ -13,22 +13,18 @@ module "beanstalk_environment_go_infra_dev" {
 
 module "rds_go_infra_dev" {
   source = "./modules/rds"
-  allocated_storage    = var.rds_allocated_storage
-  engine               = var.rds_engine
-  engine_version       = var.rds_engine_version
-  instance_class       = var.rds_instance_class
-  apply_immediately = true
-  name                 = var.rds_db_name
-  port = var.rds_port
-  username             = var.rds_username
-  password             = var.rds_password
+  rds_allocated_storage    = var.rds_allocated_storage
+  rds_engine               = var.rds_engine
+  rds_engine_version       = var.rds_engine_version
+  rds_instance_class       = var.rds_instance_class
+  rds_db_name                 = var.rds_db_name
+  rds_port = var.rds_port
+  rds_username             = var.rds_username
+  rds_password             = var.rds_password
   # CHECK THIS
-  parameter_group_name = var.rds_parameter_group_name
+  rds_parameter_group_name = var.rds_parameter_group_name
   # NEED TO RESEARCH MORE ABOUT THESE THEREE
-  db_subnet_group_name = var.rds_db_subnet_group_name
+  rds_db_subnet_group_name = var.rds_db_subnet_group_name
   # NEED MORE RESEARCH WHY EBS Inbound rules need to be addded to default 
-  vpc_security_group_ids = var.rds_vpc_security_group_ids
-  skip_final_snapshot  = true
-  deletion_protection = true 
-    
+  rds_vpc_security_group_ids = var.rds_vpc_security_group_ids  
 }
