07b9ec83206fe879ddd8864d78153a85681fa30f
Saurav <sauravads123@gmail.com>
2021-08-13 11:20:22 +0545

    improv : setup cors and basic route

--- lib/handler.go
+++ lib/handler.go
@@ -1,6 +1,9 @@
 package lib
 
 import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,5 +15,16 @@ type RequestHandler struct {
 // NewRequestHandler -> creates a new request handler
 func NewRequestHandler() RequestHandler {
 	engine := gin.New()
+	engine.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"data": "Hello 世界!",
+		})
+	})
+	engine.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	}))
 	return RequestHandler{Gin: engine}
 }
